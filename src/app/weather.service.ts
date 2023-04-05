3756f05e7ee381ffbf09f3796670555cbe6d2391
kiprotich <kiprotichmichael570@gmail.com>
2023-04-05 11:55:54 +0300

    feature: adding weather service

---
+++ src/app/weather.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  private apiKey = 'YOUR_API_KEY'; // Replace with your Open Weather Map API key
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(city: string): Observable<any> {
+    const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`;
+    return this.http.get<any>(url);
+  }
+}
